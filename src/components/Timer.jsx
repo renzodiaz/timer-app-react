8fd2de274468193af7351ae122a57ed641ab49ba
Renzo Diaz <dev.renzo.diaz@gmail.com>
2026-04-09 21:53:02 -0500

    feat: Persist time on localStorage

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,12 +1,19 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import TimerDisplay from "./TimerDisplay";
 import TimerControls from "./TimerControls";
 
 const Timer = () => {
   const timerRef = useRef(null);
 
-  const [time, setTime] = useState(0);
+  const [time, setTime] = useState(() => {
+    return Number(localStorage.getItem("time") || 0);
+  });
+
   const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("time", time);
+  }, [time]);
 
   const toggleTimer = () => {
     if (isRunning) {
@@ -28,6 +35,7 @@ const Timer = () => {
     setIsRunning(false);
     setTime(0);
     timerRef.current = null;
+    localStorage.removeItem("time");
   };
 
   return (
